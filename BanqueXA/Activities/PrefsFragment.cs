26aa27d6fa49fb170719d2f97780e14918ba1af0
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2018-12-13 21:05:29 +0100

    Feat. Settings menu

---
+++ BanqueXA/Activities/PrefsFragment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Preferences;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BanqueXA.Activities
+{
+    [Activity(Label = "@string/preferences_label")]
+    public class PrefsFragment : PreferenceFragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            AddPreferencesFromResource(Resource.Xml.preferences);
+        }
+    }
+}
