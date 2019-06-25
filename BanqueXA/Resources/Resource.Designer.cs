e70a9e12ff93f6c94ad58934bf78650ef670fa20
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2019-06-25 10:45:31 +0200

    Change menu icon with Material Icons

--- BanqueXA/Resources/Resource.Designer.cs
+++ BanqueXA/Resources/Resource.Designer.cs
@@ -31,6 +31,9 @@ namespace Eni.Banque.Android
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010000
+			public const int colorControlNormal = 2130771968;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -206,13 +209,43 @@ namespace Eni.Banque.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int ic_launcher = 2130837504;
+			public const int baseline_add_24 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int ic_launcher_foreground = 2130837505;
+			public const int baseline_add_white_18 = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int ic_launcher_round = 2130837506;
+			public const int baseline_add_white_24 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int baseline_add_white_36 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int baseline_add_white_48 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int baseline_view_list_24 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int baseline_view_list_white_18 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int baseline_view_list_white_24 = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int baseline_view_list_white_36 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int baseline_view_list_white_48 = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int ic_launcher = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int ic_launcher_foreground = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int ic_launcher_round = 2130837516;
 			
 			static Mipmap()
 			{
