54ea1e9cc54bf36c7d994cb9ce605141e96f5057
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2019-06-25 16:14:04 +0200

    Add preprocessor directive for data service manager

--- BanqueXA/Services/ServiceManager.cs
+++ BanqueXA/Services/ServiceManager.cs
@@ -3,6 +3,12 @@ namespace Eni.Banque.Android.Services
 {
     public class ServiceManager
     {
+
+#if DEBUG
         public static IBanqueAsyncService DataStore = BanqueInMemService.Instance;
+#else
+        public static IBanqueAsyncService DataStore = BanqueRestService.Instance;
+#endif
+
     }
 }
